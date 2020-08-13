f1772b292f7a3b7401ba129fe6c1c6ee9904d934
ik83551 <69639362+ik83551@users.noreply.github.com>
2020-08-13 07:55:34 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Ishaan Koradia and I love to read.");
+		System.out.println("I would like to go to a UC college such as Davis,Irvine,etc.");
+		System.out.println("I think I will major in Data Science.");
+		System.out.println("As for now, I don't play many video games.");
+		System.out.println("I love hanging out with my friends.");
+	}
+}
